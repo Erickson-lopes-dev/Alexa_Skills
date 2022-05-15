ea479b67ef9e79864acd023abdd3bacbed63d70c
Erickson-lopes-dev <ofc.erickson@gmail.com>
2022-05-15 17:44:53 -0300

    deploy

--- resources/webdriver.py
+++ resources/webdriver.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.chrome.options import Options
-# import chromedriver_autoinstaller
+import chromedriver_autoinstaller
 from selenium import webdriver
 import os
 import platform
@@ -15,10 +15,10 @@ def heroku_driver() -> webdriver:
 
 
 def win_driver() -> webdriver:
-    # chromedriver_autoinstaller.install()
+    chromedriver_autoinstaller.install()
     chrome_options = Options()
 
-    # chrome_options.add_argument("-headless")
+    chrome_options.add_argument("-headless")
     chrome_options.add_argument('--log-level=3')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
@@ -30,4 +30,4 @@ def get_webdriver() -> webdriver:
     if "Windows" in platform.platform():
         return win_driver()
     else:
-        return webdriver.Chrome('/home/erickson/Alexa_Skills/resources/chromedriver')
+        return heroku_driver()
